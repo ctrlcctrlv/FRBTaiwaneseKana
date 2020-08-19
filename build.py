f15dcc8b90ee06b51bc97b3c3b066b114bfb7629
Fredrick Brennan <copypaste@kittens.ph>
2020-08-18 23:09:13 -0400

    Remove overlap

--- build.py
+++ build.py
@@ -7,5 +7,8 @@ font="FRBTaiwaneseKana"
 
 f = ff.open(font+".sfd")
 f.mergeFeature("features.fea")
+f.selection.all()
+f.unlinkReferences()
+f.removeOverlap()
 f.generate(font+".otf", flags=("opentype", "no-hints", "omit-instructions"))
 f.close()
